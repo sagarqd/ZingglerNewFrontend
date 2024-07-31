563edddec61e895f7b832366dd130e74b4981367
sagarqd <sagar@qubetadigital.com.au>
2024-07-31 16:04:13 +0530

    Delete src directory

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  // Set basename to an empty string to remove the '/free' prefix
-  basename: '', // Use an empty string instead of '/dashboard'
-  defaultPath: '',
-  fontFamily: `'Roboto', sans-serif`,
-  borderRadius: 12
-};
-
-export default config;
